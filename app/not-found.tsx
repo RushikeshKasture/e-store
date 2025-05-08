8de8c62c76287cc320133880088f956e8fc053b4
rushikesh <rushi.kasture@gmail.com>
2025-05-08 18:05:07 +0530

    feature: added loading & notfound page

---
+++ app/not-found.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src={"/images/logo.svg"}
+        alt=""
+        height={200}
+        width={200}
+        priority={true}
+      />
+      <div className="p-6 w-1/3 rounded-lg shadow-lg text-center">
+        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+        <p className="text-destructive">Page not found</p>
+        <Button
+          variant="outline"
+          className="mt-4 ml-2"
+          onClick={() => (window.location.href = "/")}
+        >
+          {" "}
+          Back to Homepage
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
