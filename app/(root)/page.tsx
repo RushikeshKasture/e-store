d12b3f27d608e5d72cdac120dd022e24c0fd73d3
rushikesh <rushi.kasture@gmail.com>
2025-05-08 15:35:23 +0530

    feature: root layout & constant setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const Homepage = () => {
+  return <>EStore</>;
+};
+
+export default Homepage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "eStore",
-  description: "Modern Ecommerce Platform",
+  title: { template: "%s | eStore", default: APP_NAME },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
