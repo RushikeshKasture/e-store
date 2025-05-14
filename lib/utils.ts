8b5166a9a29719ab5a099333711dc915938d0090
rushikesh <rushi.kasture@gmail.com>
2025-05-14 17:17:22 +0530

    feature: signup action & form added

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,3 +16,24 @@ export function formatNumberWithDecimal(num: number): string {
     ? `${integerPart}.${decimalPart.padEnd(2, "0")}`
     : `${integerPart}.00`;
 }
+
+// format Error funtion
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function formatError(error: any) {
+  if (error.name === "ZodError") {
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exist`;
+  } else {
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
+}
